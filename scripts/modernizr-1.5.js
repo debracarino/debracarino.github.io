a99d55f89c17a7a50f73872e6d355cf78f6cb55a
debracarino <dcarino@clackamas.edu>
2016-11-02 00:48:22 -0700

    Rename case2/modernizr-1.5.js to scripts/modernizr-1.5.js

--- scripts/modernizr-1.5.js
+++ scripts/modernizr-1.5.js
@@ -14,7 +14,7 @@
  * LicenseModernizr is available under the MIT and BSD licenses:
 
  * MIT License
- * Copyright (c) 2009–2010 Faruk Ate?
+ * Copyright (c) 2009â€“2010 Faruk Ate?
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
